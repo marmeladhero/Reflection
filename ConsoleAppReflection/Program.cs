23c9b17385023d906aec0265091b6bcdd110c1e7
Ilya Shtefan <marmeladhero@gmail.com>
2018-09-07 18:20:45 +0300

    Add project files.

---
+++ ConsoleAppReflection/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppReflection
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Assembly dll = Assembly.LoadFile(@"C:\Users\isht\Downloads\HelloLibrary.dll");
+                Type t = dll.GetType("HelloLibrary.MagicClass");
+                SetCorrectValueField(t, "hello");
+                var method = t.GetMethod("Hello", BindingFlags.NonPublic | BindingFlags.Static);
+                Console.Write("Write name: ");
+                string strName = Console.ReadLine();
+
+                var result = method.Invoke(null, new object[] { strName });
+                Console.WriteLine(result);
+                Console.WriteLine("Press any key");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void SetCorrectValueField(Type t, string fieldName)
+        {
+            t.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance).SetValue(t, "Hello, ");
+        } 
+    }
+}
